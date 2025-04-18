a7a7636dd6afc9837a6d4f167d9bd17a970d30ba
Abdulrahman Aljaadi <110558473+realabdul-1@users.noreply.github.com>
2025-04-18 20:16:22 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Travel-Booking-App/', // Add this line (replace with your repo name)
+  base: '/Travel-Booking-App/', 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
