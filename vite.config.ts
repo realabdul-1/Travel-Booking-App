189c8fbd43cae9bb8471b95b90d5f4a0d48d9a6c
Abdulrahman <aljaadi.am@gmail.com>
2025-04-19 18:55:04 +0300

    Major Improvements

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
   base: '/Travel-Booking-App/', 
+=======
+  base: '', // Add this line (replace with your repo name)
+>>>>>>> 6a73ce5 (Major Improvements)
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
